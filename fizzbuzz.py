29147f61da578fadd2249560b2dd9a6a26a11e84
tam <tamulamail@gmail.com>
2019-12-01 02:10:25 +0900

    i supported the code, when it is 5.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,5 +2,9 @@ def fizzbuzz_(number):
     if number % 3 == 0:
         return 'Fizz'
 
+    if number % 5 == 0:
+        return 'Buzz'
+
 
 assert fizzbuzz_(3) == 'Fizz'
+assert fizzbuzz_(5) == 'Buzz'
